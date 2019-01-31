4832c83123af9379708509844d6df50e45688783
Evie Butland <evie.butland@gmail.com>
2019-01-31 09:57:22 +0000

    add in collision dectection

--- script.js
+++ script.js
@@ -11,10 +11,12 @@ const y = canvas.height - 30;
 const dx = 2;
 const dy = -2;
 
+const ballRadius = 10;
+
 function drawBall(){
     // draw circle every 10 milliseconds
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
@@ -24,6 +26,13 @@ function draw(){
     // clear canvas to leave on trail
     ctx.clearRect(0,0, canvas.width, canvas.height);
     drawBall();
+// if ball co-ord is less than 0 then change direction on y axis equal to negative self
+    if (y + dy < canvas.height){
+        dy = -dy;
+    }
+    if (y = dy > 0){
+        dy = -dy;
+    }
    // add 2 or -2 every 10 milliseconds
    x += dx;
    y += dy;
