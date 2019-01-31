bead7bd325c81333b0c583f9eed209066516dc7e
Evie Butland <evie.butland@gmail.com>
2019-01-31 09:44:01 +0000

    creating a fucntion that moves the ball every 10 milliseconds, and annotating code

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-let canvas = document.getElementById('myCanvas');
-let ctx = canvas.getContext('2d');
+// intialise elements to make it easier to reference 
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
 
-ctx.beginPath();
-ctx.rect(20, 40, 50 ,50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+// create a fucntion that draws a cirle, and repeat this every 10 milliseconds
+setInterval(draw, 10);
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+const x = canvas.width/2;
+const y = canvas.height - 30;
 
-ctx.beginPath();
-ctx.rect(160,10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+const dx = 2;
+const dy = -2;
+
+function draw(){
+   ctx.beginPath();
+   ctx.arc(x, y, 10, 0, Math.PI*2);
+   ctx.fillStyle = "#0095DD";
+   ctx.fill();
+   ctx.closePath();
+   // add 2 or -2 ad every 10 milliseconds
+   x += dx;
+   y += dy;
+}
+
